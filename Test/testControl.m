1cec3f318e0b9777f477df9494afa267dc032a23
Jose Tolentino <61167951+josetv91@users.noreply.github.com>
2021-09-11 23:58:29 -0500

    Create testControl.m

---
+++ Test/testControl.m
@@ -0,0 +1,35 @@
+clc; clear; close all;
+
+%function [xrefp,yrefp,zrefp,xfinal,yfinal,zfinal] = Functestcontrol(initomegatotest,omegamintotest,omegamaxtotest)
+pCs = plantConstants6;
+m = pCs.m;
+g = pCs.g;
+numberMotors = 12;
+ct = pCs.ct;
+omegaSpeed = sqrt(m*g/(ct*12)); % yields 800
+initomegatotest = omegaSpeed + 1;
+%disp(initomegatotest)
+
+omegamaxM = omegaSpeed:10:omegaSpeed + 500;
+omegamaxM = omegamaxM(2:end); % not choosing the first value
+omegaminM = omegaSpeed:-10:omegaSpeed - 500;
+omegaminM = omegaminM(2:end); % not choosing the first value
+size(omegaminM);
+count = 0;
+omegatotalminmax = [];
+for i=1:length(omegaminM)
+    for j=1:length(omegamaxM)
+        omegamintotest = omegaminM(i);
+        omegamaxtotest = omegamaxM(j);
+        try
+            [xrefp,yrefp,zrefp,xfinal,yfinal,zfinal] = Functestcontrol(initomegatotest,omegamintotest,omegamaxtotest);
+            if abs(xrefp-xfinal)<10 && abs(yrefp-yfinal)<10 && abs(zrefp-zfinal)<10
+                omegatotalminmax(count+1,1) = omegamintotest;
+                omegatotalminmax(count+1,2) = omegamaxtotest;
+                count = count+1;
+            end
+        catch
+            
+        end
+    end
+end
